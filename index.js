727006f88714ccbb1cce6a1dde8a090631afdac7
Andrew Sayers <andrew-github.com@pileofstuff.org>
2021-08-21 09:26:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 const outputs = {
     /* will set this key...     ... to this value */
     "branch-regexp"           : "^maint-",
-    "maintenance-window-start": "2021-08-15T09:00:00Z",
-    "maintenance-window-end"  : "2021-08-15T09:15:00Z",
-    "maintenance-url"         : "https://github.com/sleepdiary/internal-tools/issues/25",
+    "maintenance-window-start": "2021-08-22T09:00:00Z",
+    "maintenance-window-end"  : "2021-08-22T10:00:00Z",
+    "maintenance-url"         : "https://github.com/sleepdiary/internal-tools/issues/32",
 };
 
 // set the outputs - do not edit below this line
