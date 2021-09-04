0626101a14e6077a5af108ce36db488a02d6f029
Andrew Sayers <andrew-github.com@pileofstuff.org>
2021-09-04 15:23:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,182 +1,180 @@
 (function() {
 
 /*
- * Change the values below to whatever you want
+ * Most planned maintenances should only need to edit these variables
  */
+const build_system_pull_request = 0; // PR that will merge recent changes in the build system
+const pull_requests = { // pull requests we need to accept during the maintenance window
+/*
+    core: 19,
+    dashboard: 28,
+    info: 13,
+    report: 11,
+*/
+};
+
+const start_at = `2021-09-05T09:00:00Z`; // time that the window will open
+const issue_id = 50; // tracking issue for this maintenance
+
+/*
+ * These might change over time, but are usually the same between runs
+ */
+const owner = `andrew-sayers`; // change to your personal repo during debugging
+
+const test_actions = JSON.stringify([ // pages that will need to be checked by hand
+    {
+        action: `check_site`,
+        title: `Front page`,
+        url: `https://sleepdiary.github.io`,
+        comment: `Check links work`,
+    },
+    {
+        action: `check_site`,
+        title: `Dashboard`,
+        url: `https://sleepdiary.github.io/dashboard`,
+        comment: `Check you can load a diary`,
+    },
+    {
+        action: `check_site`,
+        title: `Notifications`,
+        url: `https://github.com/notifications`,
+        comment: `Shouldn't have any news`
+    },
+    {
+        action: `check_site`,
+        title: `Forum`,
+        url: `https://github.com/sleepdiary/sleepdiary.github.io/discussions`,
+        comment: `Shouldn't be any problems`,
+    },
+    {
+        action: `check_site`,
+        title: `GitHub Status`,
+        url: `https://www.githubstatus.com/`,
+        comment: `Should be 'All Systems Operational'`,
+    },
+]);
+
+// every repo in the organisation:
+const all_repos = [
+    `core`,
+    `dashboard`,
+    `docs`,
+    `info`,
+    `internal-tools`,
+    `report`,
+    `sleepdiary.github.io`
+];
+
+// repos with workflows that are liable to break when we update the build system:
+const workflows_that_use_the_build_system = [
+    `core`,
+    `dashboard`,
+    `info`,
+    `report`,
+];
+
 const data = {
-    /* will set this key...     ... to this value */
-    "branch-regexp"           : "^maint-",
-    "maintenance-window-start": "2021-08-29T09:00:00Z",
-    "maintenance-window-end"  : "2021-08-29T10:00:00Z",
-    "maintenance-url"         : "https://github.com/sleepdiary/internal-tools/issues/48",
-    "maintenance-actions"     : [
+    "maintenance-window-start"   : start_at,
+    "maintenance-window-duration": 1 * 60*60*1000, // one hour
+    "maintenance-url"            : `https://github.com/sleepdiary/internal-tools/issues/${issue_id}`,
+    "maintenance-steps"          : [
         {
-            "title": "Enable the announcer workflow",
-            "type": "enable_workflows",
-            "workflows": [
-                { "owner": "sleepdiary", "repo": "internal-tools", "id": "maintenance-timer.yml" },
-            ],
-        },
-        {
-            "title": "Open the maintenance window",
-            "type": "create_comments",
-            "comments": [
+            title: `Open the maintenance window`,
+            actions: [
                 {
-                    "title": "Announce the start",
-                    "owner": "sleepdiary",
-                    "repo": "internal-tools",
-                    "body": `# Maintenance procedure starting
+                    action: `add_comment`,
+                    id: issue_id,
+                    owner,
+                    repo: `internal-tools`,
+                    body: `# Maintenance procedure starting
 
 The maintenance window is [now open](https://sleepdiary.github.io/internal-tools/maintenance-actions.html).  Only maintenance-related pull requests are allowed.`,
                     fields: [],
                 },
-            ],
+                {
+                    action: `enable_workflow`,
+                    owner,
+                    repo: `internal-tools`,
+                    id: `maintenance-timer.yml`,
+                },
+            ].concat(all_repos.map( repo => ({
+                action: `create_branch`,
+                source: `main`,
+                destination: `last-known-good`,
+                owner,
+                repo
+            }))),
         },
         {
-            "title": "Create 'last-known-good' branches",
-            "type": "create_branches",
-            "branches": [
-                { "owner": "sleepdiary", "source": "main", "destination": "last-known-good", "repo": "core" },
-                { "owner": "sleepdiary", "source": "main", "destination": "last-known-good", "repo": "dashboard" },
-                { "owner": "sleepdiary", "source": "main", "destination": "last-known-good", "repo": "docs" },
-                { "owner": "sleepdiary", "source": "main", "destination": "last-known-good", "repo": "info" },
-                { "owner": "sleepdiary", "source": "main", "destination": "last-known-good", "repo": "internal-tools" },
-                { "owner": "sleepdiary", "source": "main", "destination": "last-known-good", "repo": "report" },
-                { "owner": "sleepdiary", "source": "main", "destination": "last-known-good", "repo": "sleepdiary.github.io" },
-            ],
+            title: `Pre-flight checks`,
+            actions: JSON.parse(test_actions),
         },
         {
-            "title": "Pre-flight checks",
-            type: "check_sites",
-            "sites": [
-                {
-                    title: "Front page",
-                    url: "https://sleepdiary.github.io",
-                    comment: "Check links work",
-                },
-                {
-                    title: "Dashboard",
-                    url: "https://sleepdiary.github.io/dashboard",
-                    comment: "Check you can load a diary",
-                },
-                {
-                    title: "Notifications",
-                    url: "https://github.com/notifications",
-                    comment: "Shouldn't have any news"
-                },
-                {
-                    title: "Forum",
-                    url: "https://github.com/sleepdiary/sleepdiary.github.io/discussions",
-                    comment: "Shouldn't be any problems",
-                },
-                {
-                    title: "GitHub Status",
-                    url: "https://www.githubstatus.com/",
-                    comment: "Should be 'All Systems Operational'",
-                },
-            ],
+            title: `Build and publish the new build system`,
+            actions: build_system_pull_request ? [
+                { action: `wait_for_workflow`, owner, repo: `internal-tools`, id: `autobuild.yml`, },
+                { action: `accept_pr`, owner, repo: `internal-tools`, id: build_system_pull_request, },
+            ] : [],
         },
         {
-            "title": "Build and publish the new build system",
-            "type": "accept_prs",
-            "prs": [
-                { "owner": "sleepdiary", "repo": "internal-tools", "id": 49, },
-            ],
+            title: `Update repo's`,
+            actions: (
+                Object.keys(pull_requests)
+                    .map( repo => ({ action: `accept_pr`, owner, repo, id: pull_requests[repo] }) )
+                    .concat(
+                        Object.keys(pull_requests)
+                            .map( repo => ({ action: `wait_for_workflow`, owner, repo, id: "main.yml" }) )
+                    )
+            ),
         },
         {
-            "title": "Update repo's",
-            "type": "accept_prs",
-            "prs": [
-                { "owner": "sleepdiary", "repo": "core", "id": 19, },
-                { "owner": "sleepdiary", "repo": "dashboard", "id": 28, },
-                { "owner": "sleepdiary", "repo": "info", "id": 13, },
-                { "owner": "sleepdiary", "repo": "report", "id": 11, },
-            ],
-        },
-        /* Done implicitly by the PRs in the previous step
-        {
-            "title": "Check workflows",
-            "type": "run_workflows",
-            "workflows": [
-                { "owner": "sleepdiary", "repo": "core", "id": "main.yml" },
-                { "owner": "sleepdiary", "repo": "info", "id": "main.yml" },
-                { "owner": "sleepdiary", "repo": "report", "id": "main.yml" },
-            ],
-        },
-        */
-        {
-            "title": "Check for problems",
-            type: "check_sites",
-            "sites": [
-                {
-                    title: "Front page",
-                    url: "https://sleepdiary.github.io",
-                    comment: "Check links work",
-                },
-                {
-                    title: "Dashboard",
-                    url: "https://sleepdiary.github.io/dashboard",
-                    comment: "Check you can load a diary",
-                },
-                {
-                    title: "Notifications",
-                    url: "https://github.com/notifications",
-                    comment: "Shouldn't have any news"
-                },
-                {
-                    title: "Forum",
-                    url: "https://github.com/sleepdiary/sleepdiary.github.io/discussions",
-                    comment: "Shouldn't be any problems",
-                },
-            ],
+            title: `Test-run workflows`,
+            actions: (build_system_pull_request?workflows_that_use_the_build_system:[])
+                .filter( repo => !pull_requests[repo] )
+                .map( repo => ({ action: `run_workflow`, owner, repo, id: `main.yml` }) ),
         },
         {
-            "title": "Close the maintenance window",
-            "type": "create_comments",
-            "comments": [
+            title: `Check for breakage`,
+            actions: JSON.parse(test_actions),
+        },
+        {
+            title: `Close the maintenance window`,
+            actions: all_repos.map( repo => ({
+                action: `delete_branch`,
+                name: `last-known-good`,
+                owner,
+                repo
+            })).concat([
                 {
-                    "title": "Announce the end",
-                    "owner": "sleepdiary",
-                    "repo": "internal-tools",
-                    "body": "# Maintenance finished: {{conclusion}}\n\n{{comments}}",
-                    "fields": [
+                    action: `disable_workflow`,
+                    owner,
+                    repo: `internal-tools`,
+                    id: `maintenance-timer.yml`,
+                },
+                {
+                    action: `add_comment`,
+                    id: issue_id,
+                    owner,
+                    repo: `internal-tools`,
+                    body: `# Maintenance finished: {{conclusion}}\n\n{{comments}}`,
+                    fields: [
                         {
-                            "label": "Conclusion",
-                            "id": "conclusion",
-                            "type": "switch",
-                            "on": "success",
-                            "off": "reverted",
-                            "value": false,
+                            label: `Conclusion`,
+                            id: `conclusion`,
+                            type: `switch`,
+                            on: `success`,
+                            off: `reverted`,
+                            value: false,
                         },
                         {
-                            "label": "Comments",
-                            "id": "comments",
-                            "type": "textarea",
-                            "value": "",
+                            label: `Comments`,
+                            id: `comments`,
+                            type: `textarea`,
+                            value: ``,
                         },
                     ],
                 },
-            ],
-        },
-        {
-            "title": "Delete 'last-known-good' branches",
-            "type": "delete_branches",
-            "branches": [
-                { "owner": "sleepdiary", "name": "last-known-good", "repo": "core" },
-                { "owner": "sleepdiary", "name": "last-known-good", "repo": "dashboard" },
-                { "owner": "sleepdiary", "name": "last-known-good", "repo": "docs" },
-                { "owner": "sleepdiary", "name": "last-known-good", "repo": "info" },
-                { "owner": "sleepdiary", "name": "last-known-good", "repo": "internal-tools" },
-                { "owner": "sleepdiary", "name": "last-known-good", "repo": "report" },
-                { "owner": "sleepdiary", "name": "last-known-good", "repo": "sleepdiary.github.io" },
-            ],
-        },
-        {
-            "title": "Disable the announcer workflow",
-            "type": "disable_workflows",
-            "workflows": [
-                { "owner": "sleepdiary", "repo": "internal-tools", "id": "maintenance-timer.yml" },
-            ],
+            ]),
         },
     ],
 };
@@ -193,4 +191,3 @@ The maintenance window is [now open](https://sleepdiary.github.io/internal-tools
 )(data);
 
 })();
-
